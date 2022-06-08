334f775e0b59070e1ebaca9696755430e07c70cc
Fran C <fran@example.com>
2022-06-07 21:13:34 -0400

    Lab_4

---
+++ app/src/main/java/com/example/androidlabs/Item.java
@@ -0,0 +1,12 @@
+public class Item {
+
+    String text;
+    boolean urgent;
+
+    public Item(String text, boolean urgent){
+        this.text = text;
+        this.urgent = urgent;
+    }
+
+}
+
